f444c081d5d68ad8ec4f48580bfc50fb29d908a1
Filipor <filipor123@gmail.com>
2024-12-07 03:43:57 +0100

    removed redunant code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
-import { ConfigModule } from '@nestjs/config';
-import { config } from 'src/config/config';
 
 @Module({
-  imports: [
-    WeatherModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [config],
-    }),
-  ],
+  imports: [WeatherModule],
   controllers: [AppController],
   providers: [AppService],
 })
